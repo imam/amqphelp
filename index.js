34d1d69b5442f9673fdf1ee8a61e0f2988a28288
Moonhint <moon.hint@gmail.com>
2018-01-06 10:29:52 +0700

    fix settings injector

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 const pathfinder = require('./pathfinder');
 const path_to_default_setttings = `${pathfinder.to_configs()}/default_settings.json`;
 
-let settings = require(`${path_to_default_setttings}`);
+let default_settings = require(`${path_to_default_setttings}`);
 
 module.exports = (overwrite_settings=null) => {
   let amqphelp_client;
   // if no specified settings use default settings
-  if (overwrite_settings=null){
-    amqphelp_client = require(`${pathfinder.to_app()}`)(overwrite_settings);
+  if (overwrite_settings===null){
+    amqphelp_client = require(`${pathfinder.to_app()}`)(default_settings)
   }else{
-    amqphelp_client = require(`${pathfinder.to_app()}`)(settings)
+    amqphelp_client = require(`${pathfinder.to_app()}`)(overwrite_settings);
   }
   return amqphelp_client;
 }
