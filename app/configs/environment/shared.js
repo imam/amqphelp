9c5eb339652f7191b0055710830ccecf1ad376b6
Imam Assidiqqi <assidiqqi.imam@gmail.com>
2018-03-12 14:02:32 +0700

    Adding environment variable for development

---
+++ app/configs/environment/shared.js
@@ -0,0 +1,15 @@
+module.exports = {
+    amqphelp: {
+        "connection": {
+            "host": process.env.WEB_BROKER_NAME,
+            "options": {
+                "name": process.env.WEB_BROKER_NAME,
+                "cookie": process.env.WEB_BROKER_COOKIE,
+                "user": process.env.WEB_BROKER_DEFAULT_USER,
+                "pass": process.env.WEB_BROKER_DEFAULT_PASS,
+                "vhost": process.env.WEB_BROKER_DEFAULT_VHOST,
+                "reconnect": true
+            }
+        }
+    }
+}
