307d9302d6159606dd749611da81082d5375762f
Imam Assidiqqi <assidiqqi.imam@gmail.com>
2018-03-12 14:02:57 +0700

    Promisify mongo db attacher

--- dist/attachers/mongodb.js
+++ dist/attachers/mongodb.js
@@ -6,30 +6,53 @@ var _bind2 = _interopRequireDefault(_bind);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; } // const Bind = require('./bind.js')
+
+
 module.exports = class MongoDB extends _bind2.default {
 
     create(queue_name, schema, amqp) {
-        schema.pre('save', function (next) {
-            amqp.actions.send(queue_name, this);
-            next();
+        return new Promise((resolve, reject) => {
+            schema.pre('save', (() => {
+                var _ref = _asyncToGenerator(function* (next) {
+                    yield amqp.actions.send(queue_name, this);
+                    next();
+                    resolve();
+                });
+
+                return function (_x) {
+                    return _ref.apply(this, arguments);
+                };
+            })());
         });
     }
 
     update(queue_name, schema, amqp) {
-        let self = this;
-        schema.post('update', function () {
-            amqp.actions.send(queue_name, {
-                id: self._conditions._id,
-                update: self._update
-            });
+        return new Promise((resolve, reject) => {
+            schema.post('update', _asyncToGenerator(function* () {
+                yield amqp.actions.send(queue_name, {
+                    id: this._conditions._id,
+                    update: this._update
+                });
+                resolve();
+            }));
         });
     }
 
     delete(queue_name, schema, amqp) {
-        schema.post('remove', function () {
-            amqp.actions.send(queue_name, this);
-        });
+        return new Promise((() => {
+            var _ref3 = _asyncToGenerator(function* (resolve) {
+                schema.post('remove', _asyncToGenerator(function* () {
+                    yield amqp.actions.send(queue_name, this);
+                    resolve();
+                }));
+            });
+
+            return function (_x2) {
+                return _ref3.apply(this, arguments);
+            };
+        })());
     }
 
-}; // const Bind = require('./bind.js')
+};
 //# sourceMappingURL=mongodb.js.map
